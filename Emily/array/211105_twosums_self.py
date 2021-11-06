931b63b52f3f47d63df0a01ae132a2043145373a
Emily <41024279+Ahviisagoodcat@users.noreply.github.com>
2021-11-05 21:10:10 -0400

    Update 211105_twosums_self.py
    update a better one

--- Emily/array/211105_twosums_self.py
+++ Emily/array/211105_twosums_self.py
@@ -13,3 +13,42 @@ class Solution(object):
                   
 #Runtime: 3576 ms, faster than 25.73% of Python online submissions for Two Sum.
 #Memory Usage: 14.3 MB, less than 48.96% of Python online submissions for Two Sum.
+
+
+        diff=[]
+        for i in range(len(nums)):
+            diff.append(target-nums[i])
+        
+        comm=list(set(diff).intersection(nums))
+                
+        #return comm
+        if len(comm)==2: 
+            for j in range(len(nums)):
+                if nums[j]==comm[0]:
+                    loc1=j
+
+            for k in range(len(nums)):
+                if nums[k]==comm[1]:
+                    loc2=k
+            return [loc1,loc2]
+        
+        else: 
+            if len(comm)==1:
+                loc_list=list()
+                for m in range(len(nums)):
+                    if nums[m]==target/2:
+                        loc_list.append(m)
+                return loc_list
+            else:
+                comm.remove(target/2)
+                for j in range(len(nums)):
+                    if nums[j]==comm[0]:
+                        loc1=j
+
+                for k in range(len(nums)):
+                    if nums[k]==comm[1]:
+                        loc2=k
+                return [loc1,loc2]
+            
+#Runtime: 52 ms, faster than 71.15% of Python online submissions for Two Sum.
+#Memory Usage: 14.5 MB, less than 24.56% of Python online submissions for Two Sum.
