b1a6f359d9c6e6f68cda48c4c3afd73710017498
Julie <30751636+Julieusa@users.noreply.github.com>
2021-11-08 23:32:53 -0800

    Update nonConstructibleChange.py

--- Julie/nonConstructibleChange.py
+++ Julie/nonConstructibleChange.py
@@ -1,3 +1,7 @@
+## Given an array of positive integres representing the values of coins in your possession, write a function that returns 
+## the minimum amount of change(the minium sum of money) that you cannot create. The given coins can have any positive integer value and aren't necessarily unique.
+## (i.e, you can have multiple coins of the same value)
+
 def nonConstructibleChange(coins):
     coins.sort()
 	#print(coins)
